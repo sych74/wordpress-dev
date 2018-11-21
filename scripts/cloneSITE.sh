fb2995ef21bbccfc2d43cb16477a332295cbbabc
sych74 <sych74@gmail.com>
2018-11-21 14:26:36 +0200

    Update cloneSITE.sh

--- scripts/cloneSITE.sh
+++ scripts/cloneSITE.sh
@@ -6,8 +6,8 @@ GIT_TOKEN=$3
 GIT_BRANCH=$4
 
 ROOT_DIR=/var/www/webroot/ROOT;
-rm -rf ${ROOT_DIR}
+[ -d "$ROOT_DIR" ] && rm -rf ${ROOT_DIR}
 
-host=$(echo "${GIT_URL}" |sed "s/:\/\//&${GIT_USER}:${GIT_TOKEN}@/")
+HOST=$(echo "${GIT_URL}" |sed "s/:\/\//&${GIT_USER}:${GIT_TOKEN}@/")
 git clone -b "${GIT_BRANCH}" --single-branch ${HOST} ${ROOT_DIR}
 chown nginx:nginx -hR ${ROOT_DIR}
