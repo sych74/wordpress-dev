7a70c4585515156229be8416805dffdec054e1d3
sych74 <sych74@gmail.com>
2018-11-21 14:20:31 +0200

    Update changeDomainON.sh

--- scripts/changeDomainON.sh
+++ scripts/changeDomainON.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+NEW_URL
+WP=`which wp`
+ROOT_DIR=/var/www/webroot/ROOT
+CURRENT_URL=$(echo "select option_value from wp_options where option_name='siteurl'" | ${WP} --path=${ROOT_DIR} db query)
+${WP} search-replace '${CURRENT_URL}' '${NEW_URL}' --all-tables --path=${ROOT_DIR}
